7bf5cc0748425fe6a5c1b26c2b58efc8b27b333d
SmokeTagger <126251354+SmokeTagger@users.noreply.github.com>
2023-10-24 13:54:19 +0100

    Added Small test Script

---
+++ Racing Game Preproduction Project/Assets/Scripts/testScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Test Text for Git Hub Repo");
+        
+    }
+}
