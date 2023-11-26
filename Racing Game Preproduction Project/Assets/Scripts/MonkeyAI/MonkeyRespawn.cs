6372e7cb238998c3f2064abe1dfb5720e1bf9208
Jay Kennedy <147036716+Qotsifeud@users.noreply.github.com>
2023-11-26 15:29:09 +0000

    Bug Fix: Monkey Respawn System
    Fixed (or at least think I've fixed. It'll probably find a way to prove me wrong) the bug with the Monkey/Alien spawning atop the player at the checkpoints. Will now search in the code for the player's respawn point and won't allow the respawn point for the monkey to be set until it's different from the player's.

--- Racing Game Preproduction Project/Assets/Scripts/MonkeyAI/MonkeyRespawn.cs
+++ Racing Game Preproduction Project/Assets/Scripts/MonkeyAI/MonkeyRespawn.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 
 public class MonkeyRespawn : MonoBehaviour
 {
-    //public static respawn playerInfo;
+    public GameObject playerRespawnPoint;
     public static GameObject respawnPoint;
-    //public GameObject playerRespawnPoint = playerInfo.respawnPoint;
+    int randomRespawnNumber;
     GameObject[] resPoints;
 
     public void Start()
     {
         // Reads in the number of checkpoints placed on a map at any time and adjusts the array to an appropriate size to store them.
-        resPoints = new GameObject[GameObject.Find("Check Points").transform.childCount]; 
+        //resPoints = new GameObject[GameObject.Find("Check Points").transform.childCount];
 
-        for (int i = 0; i < resPoints.Length; i++) 
+        resPoints = new GameObject[GameObject.Find("Check Points").transform.childCount];
+
+        playerRespawnPoint = GameObject.Find("Goblin Car Mk .4").GetComponent<respawn>().respawnPoint;
+
+        for (int i = 0; i < resPoints.Length; i++)
         {
-            if(i == 0)
+            if (i == 0)
             {
                 // Assigns position 0 in the array to Check Point One.
-                resPoints[i] = GameObject.Find("Check Point 1"); 
+                resPoints[i] = GameObject.Find("Check Point 1");
             }
             else
             {
                 // Assigns every other Check Point to a position.
-                resPoints[i] = GameObject.Find("Check Point " + (i+1));
+                resPoints[i] = GameObject.Find("Check Point " + (i + 1));
             }
         }
 
-        // Any time the Monkey/Alien (Monkey-lien?) becomes active and this script starts, it spawns at a random checkpoint.
         respawnPoint = resPoints[Random.Range(0, resPoints.Length)];
+
+        if(respawnPoint == playerRespawnPoint)
+        {
+            do
+            {
+                respawnPoint = resPoints[Random.Range(0, resPoints.Length)];
+            } while (respawnPoint == playerRespawnPoint);
+        }
+        
+
+        // Any time the Monkey/Alien (Monkey-lien?) becomes active and this script starts, it spawns at a random checkpoint.
+        //respawnPoint = resPoints[Random.Range(0, resPoints.Length)];
+
     }
 
     // Using this method as a way to call to this script outside of it and set the monkey to active again in a position based on the random checkpoints.
