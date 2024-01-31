ed5645970dfb0d6dfb3971636d088da18f81a568
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-01-31 11:43:52 +0000

    Minimap update and AI removal
    removed the old mini map scripts, in the process of making an updated script and better visuals, removed most of the AI characters from the scene will convert it into lightning bolt after minimap complete.

--- Racing Game Preproduction Project/Assets/Scripts/MiniMap Scripts/MinimapUI.cs
+++ Racing Game Preproduction Project/Assets/Scripts/MiniMap Scripts/MinimapUI.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.WebCam;
 
-public class MMapIconRotation : MonoBehaviour
+public class MinimapUI : MonoBehaviour
 {
 
     Transform MiniMapCam;//the camera in the main menu
     // Start is called before the first frame update
     void Start()
-    {
+    {//whatever the tag for the minimap camera is in the game...
         MiniMapCam = GameObject.FindGameObjectWithTag("MMapCam").transform;//getting the transform of the menu camera
 
     }
 
-    // Update is called once per frame
-    // Update is called once per frame
+    //this makes sure that the icons face the camera no matter the rotation of the player...
     void Update()
     {
         Vector3 direction = (MiniMapCam.position - transform.position).normalized;//gets the direction of the menu camera
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));//making the prompt face the direction of the men cam on x and z axis and y as we want it to tilt up towars the camera and not the player character
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));//making the prompt face the direction of the minimap cam on x and z axis and y as we want it to tilt up towars the camera and not the player character
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);//turns the prompt face the mencam
     }
 }
+
