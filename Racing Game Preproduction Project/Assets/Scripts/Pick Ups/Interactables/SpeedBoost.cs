dc1673ceb9c1ad598cbb076d37229ba89bb8c72e
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-02-13 16:57:20 +0000

    Speed Boost Working
    Updated Speed Boost

--- Racing Game Preproduction Project/Assets/Scripts/Pick Ups/Interactables/SpeedBoost.cs
+++ Racing Game Preproduction Project/Assets/Scripts/Pick Ups/Interactables/SpeedBoost.cs
@@ -1,48 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditorInternal.ReorderableList;
 
 public class SpeedBoost : MonoBehaviour
 {//make sure to increase acceleration not the cars boost...
-    public float smallCarBoost = 5;
-    public float mediumCarBoost = 10;
-    public float largeCarBoost = 15;
-    public float BoostTimer = 10;//10 seconds?
+    public int smallCarBoost = 90;
+    public int mediumCarBoost = 80;
+    public int largeCarBoost = 70;
 
+    public bool BoostActive = false;
 
 
     //resetting the values back to the default after the boost
-    private int SmallCarDefault;
-    private int mediumCarDefault;
-    private int largeCarDefault;    
-    
+    public int SmallCarDefault = 80;
+    public int mediumCarDefault = 70;
+    public int largeCarDefault = 60;
 
 
+   
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == ("Small Car"))
+        if (other.gameObject.tag == ("SpeedBooster") && this.gameObject.tag == ("Small Car"))
         {
-            other.gameObject.GetComponent<DriftController>().Accel = smallCarBoost;
-            Destroy(this.gameObject);
-            StartCoroutine(SpeedBoostTimer());//waits 10 seconds before resetting the acceleration to default
-
+            Destroy(other.gameObject);
+            StartCoroutine(SpeedBoostTimerSmallCar(SmallCarDefault, smallCarBoost));
         }
 
-        if (other.gameObject.tag == ("Medium Car"))
+        if (other.gameObject.tag == ("SpeedBooster") && this.gameObject.tag == ("Medium Car"))
         {
-            other.gameObject.GetComponent<DriftController>().Accel = mediumCarBoost;
-            Destroy(this.gameObject);
-            StartCoroutine(SpeedBoostTimer());//waits 10 seconds before resetting the acceleration to default
+            Destroy(other.gameObject);
+            StartCoroutine(SpeedBoostTimerSmallCar(mediumCarDefault, mediumCarBoost));
 
         }
         
-        if (other.gameObject.tag == ("Large Car"))
+        if (other.gameObject.tag == ("SpeedBooster") && this.gameObject.tag == ("Large Car"))
         {
-
-            other.gameObject.GetComponent<DriftController>().Accel = largeCarBoost;
-            Destroy(this.gameObject);
-            StartCoroutine(SpeedBoostTimer());//waits 10 seconds before resetting the acceleration to default
-
+            Destroy(other.gameObject);
+            StartCoroutine(SpeedBoostTimerSmallCar(largeCarDefault, largeCarBoost));
         }
 
 
@@ -50,15 +45,65 @@ public class SpeedBoost : MonoBehaviour
     }
 
 
-    IEnumerator SpeedBoostTimer()
+
+
+
+
+
+
+    //private void Wraper()
+    //{
+    //    StartCoroutine(SpeedBoostTimer());
+    //}
+
+
+    IEnumerator SpeedBoostTimerSmallCar(float defaut, float boost)
     {
-        yield return new WaitForSeconds(BoostTimer);//wait for 10 seconds
-        
+
+        this.gameObject.GetComponent<DriftController>().TopSpeed = boost;
+
+        Debug.Log("is being used");
+        yield return new WaitForSeconds(10);//wait for 10 seconds!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Debug.Log("is used");
+
+
+
+        this.gameObject.GetComponent<DriftController>().TopSpeed = defaut;
+
+
     }
 
+    IEnumerator SpeedBoostTimerMediumCar(float defaut, float boost)
+    {
+
+        this.gameObject.GetComponent<DriftController>().TopSpeed = boost;
+
+        Debug.Log("is being used");
+        yield return new WaitForSeconds(10);//wait for 10 seconds!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Debug.Log("is used");
 
 
 
+        this.gameObject.GetComponent<DriftController>().TopSpeed = defaut;
+
+
+    }
+
+    IEnumerator SpeedBoostTimerLargeCar(float defaut, float boost)
+    {
+
+        this.gameObject.GetComponent<DriftController>().TopSpeed = boost;
+
+        Debug.Log("is being used");
+        yield return new WaitForSeconds(10);//wait for 10 seconds!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Debug.Log("is used");
+
+
+
+        this.gameObject.GetComponent<DriftController>().TopSpeed = defaut;
+
+
+    }
 
 
 
