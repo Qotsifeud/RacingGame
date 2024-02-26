01c32fcc421ce3fc5f9557f346b1b8553734c84d
SmokeTagger <scallapazeet@gmail.com>
2024-02-26 19:09:24 +0000

    Start to menu CameraMovement

---
+++ Racing Game Preproduction Project/Assets/Scripts/Main Menu/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3[] cameraLocations;
+    public Vector3[] cameraAngles;
+
+    Vector3 targetLocation;
+    Vector3 targetAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void MoveCamera()
+    {
+        
+    }
+
+    private void RotateCamera()
+    {
+
+    }
+}
