9b110ae4fdb457e81cabab2af616e75e4e22e26b
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-02-27 17:27:15 +0000

    tweaks to the scene

--- Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/OilSpill.cs
+++ Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/OilSpill.cs
@@ -10,9 +10,21 @@ public class OilSpill : MonoBehaviour
 {
 
     public bool hastriggeredOil;
+    private DriftController driftControllerScript;
+    private float currentslipStatus;
+
+    //values added onto default
+    private float largeCarSlipValue = 100;
+    private float mediumCarSlipValue = 100;
+    private float smallCarSlipValue = 100;
 
     private void Start()
     {
+
+        driftControllerScript = this.gameObject.GetComponent<DriftController>();
+        currentslipStatus = driftControllerScript.SlipMod;
+
+
         hastriggeredOil = false;
     }
 
@@ -20,19 +32,34 @@ public class OilSpill : MonoBehaviour
     private void OnTriggerEnter(Collider Oil) //collision detection
     {
 
-        if (Oil.gameObject.tag == "Oil" && hastriggeredOil == false) 
+        if (Oil.gameObject.tag == "Oil" && hastriggeredOil == false && this.gameObject.tag == ("Small Car")) 
         {
             hastriggeredOil = false;
 
             //code for effecting car turning
+            StartCoroutine(OilSpinnerSmall());
+
+            //Destroy(Oil.gameObject);
+        }
 
 
+        if (Oil.gameObject.tag == "Oil" && hastriggeredOil == false && this.gameObject.tag == ("Medium Car"))
+        {
+            hastriggeredOil = false;
+
+            //code for effecting car turning
+            StartCoroutine(OilSpinnerMedium());
+
+            //Destroy(Oil.gameObject);
+        }
 
 
+        if (Oil.gameObject.tag == "Oil" && hastriggeredOil == false && this.gameObject.tag == ("Large Car"))
+        {
+            hastriggeredOil = false;
 
-
-
-
+            //code for effecting car turning
+            StartCoroutine(OilSpinnerLarge());
 
             //Destroy(Oil.gameObject);
         }
@@ -40,6 +67,62 @@ public class OilSpill : MonoBehaviour
 
 
     }
+
+    //this works for now
+    //TODO: add in the slide physics to the wheels???
+    IEnumerator OilSpinnerSmall()
+    { 
+        //temp change of car settings...
+        Debug.Log("currently spinning");
+
+        currentslipStatus += currentslipStatus * smallCarSlipValue;
+
+        yield return new WaitForSeconds(4);
+        //resetting to defauls drivning settings for this car type..
+
+
+
+
+        Debug.Log("No longer spinning");
+
+
+    }
+    IEnumerator OilSpinnerMedium()
+    {
+        //temp change of car settings...
+        Debug.Log("currently spinning");
+
+        currentslipStatus += currentslipStatus * mediumCarSlipValue;
+
+        yield return new WaitForSeconds(4);
+        //resetting to defauls drivning settings for this car type..
+
+
+
+
+        Debug.Log("No longer spinning");
+
+
+    }
+    IEnumerator OilSpinnerLarge()
+    {
+        //temp change of car settings...
+        Debug.Log("currently spinning");
+
+        currentslipStatus += currentslipStatus * largeCarSlipValue;
+
+        yield return new WaitForSeconds(4);
+        //resetting to defauls drivning settings for this car type..
+
+
+
+
+        Debug.Log("No longer spinning");
+
+
+    }
+
+
 
 
 }
