c7a84f32d95cc14117594dc2ad01685212f64dfd
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-02-28 09:55:47 +0000

    Used the force
    Added in a shrapnel script which adds force to the broken wall objects making it more aesthetically pleasing where as before you just broke through and only saw the broken wall for a split second.

---
+++ Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/Shrapnel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrapn : MonoBehaviour
+{
+    private Rigidbody thisBody;
+    public float useTheForce = 10;
+
+
+    private void Start()
+    {
+        
+        thisBody = this.gameObject.GetComponent<Rigidbody>();
+        thisBody.AddForce(transform.forward * useTheForce, ForceMode.Impulse);
+
+        Debug.Log("should be using the force");
+
+
+    }
+
+}
