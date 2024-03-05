68cfcde4d57ebd9a4bcf955bc2ebf1897573ac0d
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-03-05 18:41:30 +0000

    tried to fix the car camera
    well i tried, all variables are accessible but designers WILL need to play around and find what's good and what's not

--- Racing Game Preproduction Project/Assets/Scripts/Car/cameraFollow.cs
+++ Racing Game Preproduction Project/Assets/Scripts/Car/cameraFollow.cs
@@ -13,6 +13,29 @@ public class cameraFollow : MonoBehaviour
     public float thirdPdamping = 15f;
     public float thirdProtationDamping = 15f;
 
+    //this just adjusts the position of the cameras for each car...
+    public float smallCarAdjustment = 10f;
+    public float mediumCarAdjustment = 10f;
+    public float largeCarAdjustment = 10f;
+
+    //this adjusts the distance of the camera when past the below speed/ value set...
+    public float smallCarDistanceAtSpeed = -10f;
+    public float mediumCarDistanceAtSpeed = -10f;
+    public float largeCarDistanceAtSpeed = -10f;
+
+
+    //if the current speed is greater than this value then cointinue with the code...
+    public float speedOfSmallCarWhenCamDistanceChanges = 10f;
+    public float speedOfMediumCarWhenCamDistanceChanges = 10f;
+    public float speedOfLargeCarWhenCamDistanceChanges = 10f;
+
+    //this determines with car is currently in the game and executes the code depending on the car type...
+    public bool smallCarCameraInUse = false;
+    public bool mediumCarCameraInUse = false;
+    public bool largeCarCameraInUse = false;
+    public GameObject smallCar;
+    public GameObject mediumCar;
+    public GameObject largeCar;
 
     //default variables for small car not medium or large
     public float firstPdistance = 5.5f;
@@ -28,18 +51,48 @@ public class cameraFollow : MonoBehaviour
 
 
 
+    private void Start()
+    {
+        
+        smallCar = GameObject.Find("Small Car");
+        mediumCar = GameObject.Find("Medium Car");
+        largeCar = GameObject.Find("Large Car");
+
+        if(smallCar != null)
+        {
+            smallCarCameraInUse = true;
+        }
+
+        if (mediumCar != null)
+        {
+            mediumCarCameraInUse = true;
+        }
+
+        if (largeCar != null)
+        {
+            largeCarCameraInUse = true;
+        }
+
+    }
+
+
+
+
     void Update()
     {//default third person camera
         CurrentSpeed = speed.CurrentSpeed;
-        if (DriftController.thirdPersonCamera == true)
+
+        //LARGE CAR....................................................................................................................
+
+        if (DriftController.thirdPersonCamera == true && largeCarCameraInUse == true)
         {
             
-
             Vector3 wantedPosition;
-            if (CurrentSpeed > 30)
-                thirdPdistance = 10f + (CurrentSpeed - 30) / 10;
+
+            if (CurrentSpeed > speedOfLargeCarWhenCamDistanceChanges)
+                thirdPdistance = largeCarAdjustment + (CurrentSpeed - largeCarDistanceAtSpeed) / 10;
             else
-                thirdPdistance = 10f;
+                thirdPdistance = largeCarAdjustment;
             
             if (followBehind)
                 wantedPosition = thirdPersonTarget.TransformPoint(0, thirdPheight, -thirdPdistance);
@@ -56,14 +109,11 @@ public class cameraFollow : MonoBehaviour
             }
             else transform.LookAt(thirdPersonTarget, thirdPersonTarget.up);
 
-
-
         }
 
-        else if(DriftController.thirdPersonCamera == false)
+        else if(DriftController.thirdPersonCamera == false && largeCarCameraInUse == true)
         {
 
-    
             Vector3 wantedPosition;
             if (followBehind)
                 wantedPosition = firstPersonTarget.TransformPoint(0, firstPheight, -firstPdistance);
@@ -80,12 +130,107 @@ public class cameraFollow : MonoBehaviour
             }
             else transform.LookAt(firstPersonTarget, firstPersonTarget.up);
 
+        }
 
+        //MEDIUM CAR....................................................................................................................
+
+        if (DriftController.thirdPersonCamera == true && mediumCarCameraInUse == true)
+        {
+
+            Vector3 wantedPosition;
+
+            if (CurrentSpeed > speedOfMediumCarWhenCamDistanceChanges)
+                thirdPdistance = mediumCarAdjustment + (CurrentSpeed - mediumCarDistanceAtSpeed) / 10;
+            else
+                thirdPdistance = mediumCarAdjustment;
+
+            if (followBehind)
+                wantedPosition = thirdPersonTarget.TransformPoint(0, thirdPheight, -thirdPdistance);
+            else
+                wantedPosition = thirdPersonTarget.TransformPoint(0, thirdPheight, thirdPdistance);
+
+            transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * thirdPdamping);
+
+            if (smoothRotation)
+            {
+                Quaternion wantedRotation = Quaternion.LookRotation(thirdPersonTarget.position - transform.position, thirdPersonTarget.up);
+                //Quaternion ownRotation = Quaternion.RotateTowards;
+                transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * thirdProtationDamping);
+            }
+            else transform.LookAt(thirdPersonTarget, thirdPersonTarget.up);
 
         }
 
+        else if (DriftController.thirdPersonCamera == false && mediumCarCameraInUse == true)
+        {
 
+            Vector3 wantedPosition;
+            if (followBehind)
+                wantedPosition = firstPersonTarget.TransformPoint(0, firstPheight, -firstPdistance);
+            else
+                wantedPosition = firstPersonTarget.TransformPoint(0, firstPheight, firstPdistance);
 
+            transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * firstPdamping);
+
+            if (smoothRotation)
+            {
+                Quaternion wantedRotation = Quaternion.LookRotation(firstPersonTarget.position - transform.position, firstPersonTarget.up);
+                //Quaternion ownRotation = Quaternion.RotateTowards;
+                transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * firstProtationDamping);
+            }
+            else transform.LookAt(firstPersonTarget, firstPersonTarget.up);
+
+        }
+
+        //SMALL CAR....................................................................................................................
+
+        if (DriftController.thirdPersonCamera == true && smallCarCameraInUse == true)
+        {
+
+            Vector3 wantedPosition;
+
+            if (CurrentSpeed > speedOfSmallCarWhenCamDistanceChanges)
+                thirdPdistance = smallCarAdjustment + (CurrentSpeed - smallCarDistanceAtSpeed) / 10;
+            else
+                thirdPdistance = smallCarAdjustment;
+
+            if (followBehind)
+                wantedPosition = thirdPersonTarget.TransformPoint(0, thirdPheight, -thirdPdistance);
+            else
+                wantedPosition = thirdPersonTarget.TransformPoint(0, thirdPheight, thirdPdistance);
+
+            transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * thirdPdamping);
+
+            if (smoothRotation)
+            {
+                Quaternion wantedRotation = Quaternion.LookRotation(thirdPersonTarget.position - transform.position, thirdPersonTarget.up);
+                //Quaternion ownRotation = Quaternion.RotateTowards;
+                transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * thirdProtationDamping);
+            }
+            else transform.LookAt(thirdPersonTarget, thirdPersonTarget.up);
+
+        }
+
+        else if (DriftController.thirdPersonCamera == false && smallCarCameraInUse == true)
+        {
+
+            Vector3 wantedPosition;
+            if (followBehind)
+                wantedPosition = firstPersonTarget.TransformPoint(0, firstPheight, -firstPdistance);
+            else
+                wantedPosition = firstPersonTarget.TransformPoint(0, firstPheight, firstPdistance);
+
+            transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * firstPdamping);
+
+            if (smoothRotation)
+            {
+                Quaternion wantedRotation = Quaternion.LookRotation(firstPersonTarget.position - transform.position, firstPersonTarget.up);
+                //Quaternion ownRotation = Quaternion.RotateTowards;
+                transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * firstProtationDamping);
+            }
+            else transform.LookAt(firstPersonTarget, firstPersonTarget.up);
+
+        }
 
     }
 }
