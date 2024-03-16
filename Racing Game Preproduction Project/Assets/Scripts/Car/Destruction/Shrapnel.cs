c461af451990521161321c8bd2e8d02fa4d349d0
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-03-16 15:57:12 +0000

    Obstacles complete

--- Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/Shrapnel.cs
+++ Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/Shrapnel.cs
@@ -6,15 +6,15 @@ public class Shrapn : MonoBehaviour
 {
     private Rigidbody thisBody;
     public float useTheForce = 10;
-
+   
 
     private void Start()
     {
         
         thisBody = this.gameObject.GetComponent<Rigidbody>();
         thisBody.AddForce(transform.forward * useTheForce, ForceMode.Impulse);
+        
 
-     
 
 
     }
