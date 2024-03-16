1b3fa095cace7a977e5aa47eccb673cf5969e6c7
Adon-Young <AYOUNG305@caledonian.ac.uk>
2024-03-16 06:42:27 +0000

    Fixed the toilet
    Added in the prefabs for barriers, need new models for broken prefabs though...

--- Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/WallCollision.cs
+++ Racing Game Preproduction Project/Assets/Scripts/Car/Destruction/WallCollision.cs
@@ -13,7 +13,9 @@ public class WallCollision : MonoBehaviour
     public GameObject brokenBarrel1;
     public GameObject brokenBarrel2;
     public GameObject brokenBarrel3;
-
+    //barriers...
+    public GameObject barrier1Broken;
+    public GameObject barrier2Broken;
 
 
 
@@ -22,9 +24,10 @@ public class WallCollision : MonoBehaviour
     private bool destructionModelInstantiated = false;
     private bool B1CurrentlyInstantiated = false;
     private bool B2CurrentlyInstantiated = false;
-
     private bool B4CurrentlyInstantiated = false;
 
+    private bool Bar1Instantiated = false;
+    private bool Bar2Instantiated = false;
 
 
     void Update()
@@ -106,6 +109,38 @@ public class WallCollision : MonoBehaviour
         }
 
 
+        //barriers...
+
+
+        if (Object.gameObject.tag == "Bar1" && gameObject.tag == "Large Car" && carCurrentSpeed >= driftControllerScript.halfSpeed)
+        {
+            if (!Bar1Instantiated) // Check if destruction model is not already instantiated
+            {
+                Instantiate(barrier1Broken, Object.transform.position, Object.transform.rotation);
+                Bar1Instantiated = true; // Set flag to true indicating destruction model is instantiated
+            }
+            else
+            {
+                Destroy(Object.gameObject); // Destroy the collided wall object if destruction model is already instantiated
+            }
+
+
+        }
+
+        if (Object.gameObject.tag == "Bar2" && gameObject.tag == "Large Car" && carCurrentSpeed >= driftControllerScript.halfSpeed)
+        {
+            if (!Bar2Instantiated) // Check if destruction model is not already instantiated
+            {
+                Instantiate(barrier2Broken, Object.transform.position, Object.transform.rotation);
+                Bar2Instantiated = true; // Set flag to true indicating destruction model is instantiated
+            }
+            else
+            {
+                Destroy(Object.gameObject); // Destroy the collided wall object if destruction model is already instantiated
+            }
+
+
+        }
 
 
     }
